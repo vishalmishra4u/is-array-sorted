d1208744df902376314df68e093af5b4d8b320da
vishalmishra4u <mishravishal0912@gmail.com>
2017-06-16 00:14:25 +0530

    Issues fixes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = function isArraySorted(array){
-  for(var i = 0; i < array.length, i++){
+  for(var i = 0; i < array.length; i++){
     var a = array[i];
     var b = array[i+1];
 
-    if(comaperer(a,b) > 0) ? return false;
+    if(comaperer(a,b) > 0){
+      return false;
+    }
   }
   return true;
 }
